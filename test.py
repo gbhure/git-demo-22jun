9a77e199f4cd59f7fbc848767bd6feebe8ca73a7
Ganesh Bhure <ganesh.bhure1@gmail.com>
2024-06-23 03:44:55 +0000

    main changes for apple

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print('hello from python program')
 print('end of program')
 non python code..by dev1_bugs
+new changes started here
+apple is red
+this is apple tree
+we want to pick all the apples
+new changes ends here
